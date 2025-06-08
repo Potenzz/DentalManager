b960e3762442abc1205c442cf84f85cfa3efe998
Vishnu <vishnu10kumar11@gmail.com>
2025-06-08 18:55:17 +0530

    title changed

--- apps/Frontend/src/main.tsx
+++ apps/Frontend/src/main.tsx
@@ -2,8 +2,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-document.title = "DentalConnect - Patient Management System";
-
 createRoot(document.getElementById('root')!).render(
     <App />
 )
